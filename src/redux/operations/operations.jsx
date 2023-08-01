dafff9f3ffe77c739e1db0563b8933a1ad0805d5
Vadym Zhuravlov <vadiazh@gmail.com>
2023-08-01 22:23:41 +0300

    1

---
+++ src/redux/operations/operations.jsx
@@ -0,0 +1,41 @@
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+axios.defaults.baseURL = 'https://64c947e2b2980cec85c21c03.mockapi.io/contacts';
+
+export const fetchContacts = createAsyncThunk(
+  'contacts/fetchContacts',
+  async (_, API) => {
+    try {
+      const response = await axios.get('/contacts');
+      return response.data;
+    } catch (e) {
+      return API.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const addContact = createAsyncThunk(
+  'contacts/addContact',
+  async (contact, API) => {
+    try {
+      const { name, number } = contact;
+      const response = await axios.post('/contacts', { name, number });
+      return response.data;
+    } catch (e) {
+      return API.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const deleteContact = createAsyncThunk(
+  'contacts/deleteContact',
+  async (id, API) => {
+    try {
+      const response = await axios.delete(`/contacts/${id}`);
+      return response.data;
+    } catch (e) {
+      return API.rejectWithValue(e.message);
+    }
+  }
+);
